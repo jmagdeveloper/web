45fde2a94829ac6258c8e5e4f73c7f579a9949c7
jmagdeveloper <117964380+jmagdeveloper@users.noreply.github.com>
2022-11-10 23:22:31 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>JMAG Developer</title>
+</head>
+<body>
+
+<center><img src="https://static.vecteezy.com/system/resources/previews/000/192/152/non_2x/website-under-construction-vector.png"/></center>
+<center style="margin-top:-90px;">
+<h1>Estamos en remodelaci&oacute;n pronto Volveremos</h1>
+<b>Email:</b> <a href="#" style="font-weight:700; text-decoration:none; color:blue;">contacto@jmagdeveloper.com</a>
+<b>  |  Whatsapp:</b> <a href="https://wa.link/9nn6u9" style="font-weight:700; color:blue;">wa.link/9nn6u9</a>
+<b>  |  Tel&eacute;fono:</b> <a href="#" style="font-weight:700; text-decoration:none; color:blue;">+51 967 193 525</a>
+
+</center>
+
+</body>
+</html>
+
